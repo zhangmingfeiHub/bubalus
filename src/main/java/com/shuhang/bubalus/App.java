8481eae25266cd583d4743ef1f4e31c8dfc095fd
张明飞 <mingfei.zhang@chinaredstar.com>
2018-10-19 17:35:41 +0800

    初始提交

---
+++ src/main/java/com/shuhang/bubalus/App.java
@@ -0,0 +1,13 @@
+package com.shuhang.bubalus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
